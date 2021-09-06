05aa92fb2d5cbcd3af40d800a22baac05879fbc6
ratva0717 <ratchana@student.tce.edu>
2021-09-06 18:04:14 +0530

    Backend changed from FastAPI to Flask framework, due to ease of implementation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# fastapi lib
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pathlib import Path
+from flask import Flask, render_template, request
+from scripts_driver.visualize import plot_wind, plot_temp
 
-base_path = Path(__file__).resolve().parent
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-template = Jinja2Templates(directory=str(base_path / 'templates'))
+app = Flask(__name__)
 
 
-@app.get('/airportHome')
-async def root(request: Request):
-    return template.TemplateResponse('index.html', {'request': request})
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    w_fig = plot_wind()
+    t_fig = plot_temp()
+    return render_template('index.html', ws_url=w_fig, tmp_url=t_fig)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=8000)
