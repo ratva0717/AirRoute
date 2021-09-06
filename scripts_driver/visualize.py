57f8d6b3e35f42d78d5c6ab15d76097af1bdbad3
ratva0717 <ratchana@student.tce.edu>
2021-09-06 18:07:53 +0530

    Dataset

---
+++ scripts_driver/visualize.py
@@ -0,0 +1,54 @@
+import base64
+import io
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_wind():
+    '''
+  Function to perform visualization of weather data
+  '''
+    # wind speed vs month
+    airport_df = pd.read_csv('scripts_driver/M1_final.csv')
+
+    sns.set(style='darkgrid')
+    fig, (ax1) = plt.subplots(nrows=1, figsize=(17, 5))
+    sns.barplot(
+        y='Wind Speed',
+        x='MONTH',
+        hue='DAY_OF_MONTH',
+        data=airport_df,
+        palette='light:#5A9'
+    )
+    ax1.set(ylabel="WIND SPEED",
+            xlabel="DAY OF MONTHS")
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return plot_url
+
+
+def plot_temp():
+    airport_df = pd.read_csv('scripts_driver/M1_final.csv')
+    # air pressure vs month
+    sns.set(style='white')
+    fig, ax = plt.subplots(nrows=1, figsize=(17, 5))
+    sns.barplot(
+        x='MONTH',
+        y='Pressure',
+        hue='DAY_OF_MONTH',
+        data=airport_df,
+        palette='flare',
+    )
+    ax.set(ylabel="AIR PRESSURE",
+           xlabel="DAY OF MONTHS")
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return plot_url
