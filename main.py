8696fd722d85bd6c8744e31be961e96a5f9936b0
ratva0717 <ratchana@student.tce.edu>
2021-12-07 14:49:09 +0530

    Weather classification and runway prediction

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ import time
 
 app = Flask(__name__)
 
+uploads_dir = os.path.join(app.instance_path, 'uploads')
+
 
 @app.route('/')
 def index():
     # Performence
     NB_Accuracy, NB_Precision, NB_Recall = NB_PredictionMeasure()
-    SVM_Accuracy, SVM_Precision, SVM_Recall = SVM_PredictionMeasure()
+    SVM_Accuracy, SVM_Precisio, SVM_Recall = SVM_PredictionMeasure()
 
     return render_template('index.html',
                            NB_Accuracy=NB_Accuracy, NB_Precision=NB_Precision, NB_Recall=NB_Recall,
@@ -76,10 +78,33 @@ def ts():
 
 
 @app.route('/landing', methods=['GET', 'POST'])
-def landing():
+def landing():    
     return render_template('landing.html')
 
+@app.route("/detect", methods=['POST'])
+def detect():
+    if not request.method == "POST":
+        return
+    video = request.files['video']
+    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
+    print(video)
+    subprocess.run("ls")
+    subprocess.run(['python3', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))])
 
+    # return os.path.join(uploads_dir, secure_filename(video.filename))
+    obj = secure_filename(video.filename)
+    return obj
+
+@app.route('/return-files', methods=['GET'])
+def return_file():
+    obj = request.args.get('obj')
+    loc = os.path.join("runs/detect", obj)
+    print(loc)
+    try:
+        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+        # return send_from_directory(loc, obj)
+    except Exception as e:
+        return str(e)
 def get_api_key():
     config = configparser.ConfigParser()
     config.read('config.ini')
